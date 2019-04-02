5eebec85ff0073145c643ea3549be01fe791d670
sunpeng <771968690@qq.com>
2019-04-02 18:22:31 +0800

    sunpeng

--- mq-center-rabbit-admin/src/main/java/com/mq/controler/LoginControler.java
+++ mq-center-rabbit-admin/src/main/java/com/mq/controler/LoginControler.java
@@ -1,9 +1,14 @@
 package com.mq.controler;
 
+import com.mq.common.util.ValueHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * 功能：登录 controller
@@ -15,8 +20,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/login")
 public class LoginControler {
 
+    @Inject
+    private LoginService loginService;
+    @Inject
+    private SysUserRepository sysUserRepository;
+    @Inject
+    private RedisRepositoryCustom redisRepositoryCustom;
+    @Inject
+    private ValueHolder valueHolder;
+
     @GetMapping(value = "/toLogin")
     public String toLogin() {
         return "login";
+    }
+
+    @GetMapping(value = "/toSuccess")
+    public String toSuccess(HttpServletRequest request) throws Exception {
+        if (loginService.tokenValidate(request)) {
+            TbSysUser user = sysUserRepository.findOne(valueHolder.getUserIdHolder());
+            request.setAttribute("uname", user != null ? user.getUname() : "");
+            return "home";
+        }
+        return "login";
+    }
+
+    @GetMapping(value = "/out")
+    public String out(HttpServletRequest request) throws Exception {
+        if (loginService.tokenValidate(request)) {
+            redisRepositoryCustom.delete(RedisKeyUtil.getRedisUserInfoKey(valueHolder.getUserIdHolder()));
+        }
+        return "login";
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/verify")
+    public Map verify(@RequestBody @Validated(UserLoginRequest.BaseInfo.class) UserLoginRequest param) throws Exception {
+        return loginService.verify(param);
     }
 }
