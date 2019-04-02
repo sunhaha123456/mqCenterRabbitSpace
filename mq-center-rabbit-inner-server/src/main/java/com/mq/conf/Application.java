4fd369742c593859d1437bb56b84dbbdb07fe1ec
sunpeng <771968690@qq.com>
2019-04-02 15:26:28 +0800

    sunpeng

--- mq-center-rabbit-inner-server/src/main/java/com/mq/conf/Application.java
+++ mq-center-rabbit-inner-server/src/main/java/com/mq/conf/Application.java
@@ -11,9 +11,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @RestController
 @ComponentScan("com.mq")
@@ -37,8 +35,10 @@ public class Application extends WebMvcConfigurationSupport {
         return new StringRedisTemplate(connectionFactory);
     }
 
+    /*
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
+    */
 }
--- mq-center-rabbit-inner-server/src/main/java/com/mq/controler/LoginControler.java
+++ mq-center-rabbit-inner-server/src/main/java/com/mq/controler/LoginControler.java
@@ -1,17 +1,12 @@
 package com.mq.controler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 功能：登录 controller
- * @author sunpeng
- * @date 2018
- */
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/login")
 public class LoginControler {
 
