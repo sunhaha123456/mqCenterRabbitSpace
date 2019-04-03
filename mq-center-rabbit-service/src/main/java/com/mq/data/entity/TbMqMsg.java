bc9861bff5f57423e414266a631fe138fe7ca3fa
sunpeng <771968690@qq.com>
2019-04-03 16:17:55 +0800

    sunpeng

--- mq-center-rabbit-service/src/main/java/com/mq/data/entity/TbMqMsg.java
+++ mq-center-rabbit-service/src/main/java/com/mq/data/entity/TbMqMsg.java
@@ -1,6 +1,6 @@
 package com.mq.data.entity;
 
-import com.mq.common.data.base.BaseDataIdStr;
+import com.mq.common.data.base.BaseDataIdLong;
 import lombok.Data;
 
 import javax.persistence.Column;
@@ -15,7 +15,7 @@ import javax.persistence.Table;
 @Data
 @Entity
 @Table(name = "tb_mq_msg")
-public class TbMqMsg extends BaseDataIdStr {
+public class TbMqMsg extends BaseDataIdLong {
 
 	@Column(name = "msg_str", columnDefinition = "varchar(500) COMMENT '消息字符串'")
 	private String msgStr;
