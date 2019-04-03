2ac095e635b7a2e8c0fd76ec982deb41d265b3e4
sunpeng <771968690@qq.com>
2019-04-03 14:34:48 +0800

    sunpeng

---
+++ mq-center-rabbit-service/src/main/java/com/mq/data/entity/TbMqMsg.java
@@ -0,0 +1,27 @@
+package com.mq.data.entity;
+
+import com.mq.common.data.base.BaseDataIdLong;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * 功能：消息表
+ * @author sunpeng
+ * @date 2019
+ */
+@Data
+@Entity
+@Table(name = "tb_mq_msg")
+public class TbMqMsg extends BaseDataIdLong {
+
+	@Column(name = "msg_str", columnDefinition = "varchar(500) COMMENT '消息字符串'")
+	private String msgStr;
+
+	// 推送状态
+	// 0：未推送 1：推送失败 2：推送成功
+	@Column(name = "status", columnDefinition = "INT(1) DEFAULT 0 COMMENT '推送状态'")
+	private Integer status;
+}
---
+++ mq-center-rabbit-service/src/main/java/com/mq/data/entity/TbMqMsgPushReleation.java
@@ -0,0 +1,27 @@
+package com.mq.data.entity;
+
+import com.mq.common.data.base.BaseDataIdLong;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * 功能：消息表推送记录表
+ * @author sunpeng
+ * @date 2019
+ */
+@Data
+@Entity
+@Table(name = "tb_mq_msg_push_releation")
+public class TbMqMsgPushReleation extends BaseDataIdLong {
+
+	@Column(name = "mq_msg_id")
+	private Long mqMsgId;
+
+	// 推送状态
+	// 0：推送失败 1：推送成功
+	@Column(name = "status", columnDefinition = "INT(1) DEFAULT 0 COMMENT '推送状态'")
+	private Integer status;
+}
